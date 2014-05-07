edc342d6029027a7cd822f28f9600257ced6fabe
Doug Bromley <doug.bromley@gmail.com>
2014-05-07 10:41:14 +0100

    Changed from optarse to argparse

--- red2dig.py
+++ red2dig.py
@@ -1,22 +1,30 @@
 # -*- coding: utf8 -*-
 from reddittodiigo import RedditSaves
 from reddittodiigo import DiigoSaver
-import optparse
+import argparse
+import sys
 
 if __name__ == "__main__":
-    parser = optparse.OptionParser()
-    parser.add_option("-u", "--ru", dest="ruser", help="Reddit username")
-    parser.add_option("-p", "--rp", dest="rpass", help="Reddit password")
-    parser.add_option("-v", "--du", dest="duser", help="Diigo username")
-    parser.add_option("-q", "--dp", dest="dpass", help="Diigo password")
-    parser.add_option("-l", "--limit", dest="limit", default=25,
+    parser = argparse.ArgumentParser(
+        usage='Copy your saved Reddit links to a Diigo account',
+        description='Your Reddit and Diigo credentials'
+    )
+    parser.add_argument("-u", "--ru", dest="ruser", help="Reddit username")
+    parser.add_argument("-p", "--rp", dest="rpass", help="Reddit password")
+    parser.add_argument("-v", "--du", dest="duser", help="Diigo username")
+    parser.add_argument("-q", "--dp", dest="dpass", help="Diigo password")
+    parser.add_argument("-l", "--limit", dest="limit", default=25,
         help="How many saved links to process at once")
-    parser.add_option("-m", "--move", dest="unsave", default=True,
+    parser.add_argument("-m", "--move", dest="unsave", default=True,
         help="Move and delete from Reddit saves")
 
-    (options, args) = parser.parse_args()
+    args = parser.parse_args()
 
-    r = RedditSaves(options.ruser, options.rpass)
-    d = DiigoSaver(options.duser, options.dpass)
+    if not args.ruser or not args.rpass or not args.duser or not args.dpass:
+        parser.print_help()
+        sys.exit(-1)
+
+    r = RedditSaves(args.ruser, args.rpass)
+    d = DiigoSaver(args.duser, args.dpass)
 
     d.save_to_diigo(r.get_saved())
